e625bd4a31fef86f256ece4b772f73f041727070
Jacob Marshall <git@jacobmarshall.co>
2016-07-23 00:25:05 +1200

    Add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+if [ -d ./bin ]; then
+    echo "Cleaning bin directory..."
+    rm -rf ./bin/*
+fi
+
+for GOOS in darwin windows linux; do
+  for GOARCH in 386 amd64; do
+    echo "Building $GOOS/$GOARCH..."
+    FILENAME="pokevision"
+    if [ "windows" == $GOOS ]; then
+        FILENAME="pokevision.exe"
+    fi
+    GOOS=$GOOS GOARCH=$GOARCH go build -o ./bin/$GOOS/$GOARCH/$FILENAME ./
+    tar -C ./bin/$GOOS/$GOARCH/ -cvzf ./bin/$GOOS-$GOARCH.tar.gz .
+  done
+done
+
+echo "Building linux/arm..."
+GOOS=linux GOARCH=arm go build -o ./bin/linux/arm/pokevision ./
+tar -C ./bin/linux/arm/ -cvzf ./bin/linux-arm.tar.gz .
