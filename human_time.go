153ce8b0a263edce819a5df534ff89a100c5454a
Jacob Marshall <git@jacobmarshall.co>
2016-07-27 12:52:39 +1200

    Fix fatal error when pokemon expires “now”

--- human_time.go
+++ human_time.go
@@ -4,10 +4,13 @@ import (
     "github.com/dustin/go-humanize"
     "time"
     "strings"
+    "math"
 )
 
 func HumanTime(time time.Time) string {
     original := humanize.Time(time)
     parts := strings.Split(original, " ")
-    return parts[0] + " " + parts[1]
+    length := int(math.Min(float64(len(parts)), 2)) // now / x minutes
+    parts = parts[:length]
+    return strings.Join(parts, " ")
 }
