7a5845f3c0165bdb8c9738d6e5d44d48730cf47c
Tim <tim.quax@mobgen.com>
2016-07-29 13:57:10 +0200

    added pokemon icon to linux push notification

--- out_notification_linux.go
+++ out_notification_linux.go
@@ -6,7 +6,8 @@ import (
 )
 
 func OutputToNotificationCenter(pokemon PokemonMeta, config Config) error {
+    pokemonIcon, _ := DownloadIcon(pokemon.PokedexPokemon.Index)
 
-    exec.Command("notify-send", "-i", "", pokemon.Name + " located near " + pokemon.Location, "Expires in " + HumanTime(pokemon.ExpiresAt)).Run()
+    exec.Command("notify-send", "-i", pokemonIcon, pokemon.Name + " located near " + pokemon.Location, "Expires in " + HumanTime(pokemon.ExpiresAt)).Run()
     return nil
 }
