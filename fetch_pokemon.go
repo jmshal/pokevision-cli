8299f8b186e30a77d44e57db38a55a2223fd68df
JP Robinson <john.paul.robinson@gmail.com>
2016-07-29 17:06:11 -0400

    adding nil checks for is_alive and uid

--- fetch_pokemon.go
+++ fetch_pokemon.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 const (
-    GET_MAP_DATA_URL = "https://pokevision.com/map/data/%v/%v"
+	GET_MAP_DATA_URL = "https://pokevision.com/map/data/%v/%v"
 )
 
 func FetchPokemonsInRegion(lat, lon float64) ([]Pokemon, error) {
-    data, err := RequestAPI(fmt.Sprintf(GET_MAP_DATA_URL, lat, lon))
-    if err != nil {
-        return nil, err
-    }
-    rawPokemons := data["pokemon"].([]interface{})
-    pokemons := make([]Pokemon, len(rawPokemons))
-    for index, unusableRawPokemon := range rawPokemons {
-        rawPokemon := unusableRawPokemon.(map[string]interface{})
-        pokemonId := int(rawPokemon["pokemonId"].(float64))
-        pokemons[index] = Pokemon{
-            ID: int(rawPokemon["id"].(float64)),
-            UID: rawPokemon["uid"].(string),
-            ExpiresAt: time.Unix(int64(rawPokemon["expiration_time"].(float64)), 0),
-            PokedexID: pokemonId,
-            Latitude: rawPokemon["latitude"].(float64),
-            Longitude: rawPokemon["longitude"].(float64),
-            IsAlive: rawPokemon["is_alive"].(bool),
-        }
-    }
-    return pokemons, nil
+	data, err := RequestAPI(fmt.Sprintf(GET_MAP_DATA_URL, lat, lon))
+	if err != nil {
+		return nil, err
+	}
+	rawPokemons := data["pokemon"].([]interface{})
+	pokemons := make([]Pokemon, len(rawPokemons))
+	for index, unusableRawPokemon := range rawPokemons {
+		rawPokemon := unusableRawPokemon.(map[string]interface{})
+		pokemonId := int(rawPokemon["pokemonId"].(float64))
+		uid, ok := rawPokemon["uid"].(string)
+		if !ok {
+			uid = ""
+		}
+		alive, ok := rawPokemon["is_alive"].(bool)
+		if !ok {
+			alive = false
+		}
+
+		pokemons[index] = Pokemon{
+			ID:        int(rawPokemon["id"].(float64)),
+			UID:       uid,
+			ExpiresAt: time.Unix(int64(rawPokemon["expiration_time"].(float64)), 0),
+			PokedexID: pokemonId,
+			Latitude:  rawPokemon["latitude"].(float64),
+			Longitude: rawPokemon["longitude"].(float64),
+			IsAlive:   alive,
+		}
+	}
+	return pokemons, nil
 }
