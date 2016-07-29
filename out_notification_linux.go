ca865c82a253ba3fba6f5eb1d5cc9781351fa157
Tim <tim.quax@mobgen.com>
2016-07-29 14:06:28 +0200

    Added distance in linux notification

--- out_notification_linux.go
+++ out_notification_linux.go
@@ -2,12 +2,13 @@ package main
 
 
 import (
-	"os/exec"
+    "os/exec"
+    "strconv"
 )
 
 func OutputToNotificationCenter(pokemon PokemonMeta, config Config) error {
     pokemonIcon, _ := DownloadIcon(pokemon.PokedexPokemon.Index)
 
-    exec.Command("notify-send", "-i", pokemonIcon, pokemon.Name + " located near " + pokemon.Location, "Expires in " + HumanTime(pokemon.ExpiresAt)).Run()
+    exec.Command("notify-send", "-i", pokemonIcon, pokemon.Name + " - " + strconv.Itoa(pokemon.Distance) + "m from " + pokemon.Location, "Expires in " + HumanTime(pokemon.ExpiresAt)).Run()
     return nil
 }
