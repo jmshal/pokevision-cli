db6b9223cb2d81d720edda38a65add59742a0585
Tim <tim.quax@mobgen.com>
2016-07-29 12:07:56 +0200

    push notifications for linux

--- out_notification_linux.go
+++ out_notification_linux.go
@@ -1,5 +1,12 @@
 package main
 
+
+import (
+	"os/exec"
+)
+
 func OutputToNotificationCenter(pokemon PokemonMeta, config Config) error {
+
+    exec.Command("notify-send", "-i", "", pokemon.Name + " located near " + pokemon.Location, "Expires in " + HumanTime(pokemon.ExpiresAt)).Run()
     return nil
 }
